aa50a8741c12272037d06d0c1624f4d561a04313
张力 <zhangli@weipaitang.com>
2020-11-06 17:50:28 +0800

    数组去重

--- a.js
+++ a.js
@@ -1,16 +1,3 @@
-vm._watchers = [];
-const opts = vm.$options;
-//初始化所有属性
-if (opts.props) initProps(vm, opts.props);
-// 初始化回调函数
-if (opts.methods) initMethods(vm, opts.methods);
-// 数据响应化
-if (opts.data) {
-  initData(vm);
-} else {
-  observe((vm._data = {}), true);
-}
-if (opts.computed) initComputed(vm, opts.computed);
-if (opts.watch && opts.watch !== nativeWatch) {
-  initWatch(vm, opts.watch);
-}
+const arr = [1, 2, 3, 4, 11, 1];
+const res = Array.from(new Set(arr));
+const res2 = [...new Set(arr)];
