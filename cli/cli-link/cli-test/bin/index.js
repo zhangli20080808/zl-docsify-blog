ed9e7427921bf96058df6e66febacdd93246cb0d
zhangli20080808 <791492434@qq.com>
2021-06-08 23:25:37 +0800

    feat

--- cli/cli-link/cli-test/bin/index.js
+++ cli/cli-link/cli-test/bin/index.js
@@ -1,3 +1,39 @@
 #!/usr/bin/env node
-const { sum } = require('ss-lib');
-console.log(sum(1, 2, 9));
+
+// 1. 注册一个命令 init
+// 2. 实现参数解析 --version 和 init --name vue-test
+
+const argv = require('process').argv;
+const lib = require('ss-lib');
+const command = argv[2];
+// [ '/usr/local/bin/node', '/usr/local/bin/cli-test', 'init' ]
+// argv [
+//   '/usr/local/bin/node',
+//   '/usr/local/bin/cli-test',
+//   '--name',
+//   'vue-test'
+// ]
+const options = argv.slice(3);
+
+if (options.length > 1) {
+  let [option, param] = options;
+  option = option.replace('--', '');
+
+  if (command) {
+    if (lib[command]) {
+      lib[command]({ option, param });
+    } else {
+      console.log('无效的命令');
+    }
+  } else {
+    console.log('请输入命令');
+  }
+}
+
+if (command.startsWith('--') || command.startsWith('-')) {
+  let globalOption = command.replace(/--|-/g, '');
+  console.log(globalOption);
+  if (globalOption === 'version' || globalOption === 'V') {
+    console.log('1.1.1');
+  }
+}
--- cli/cli-link/ss-lib/lib/index.js
+++ cli/cli-link/ss-lib/lib/index.js
@@ -2,4 +2,7 @@ module.exports = {
   sum(a, b, c) {
     return a + b + c;
   },
+  init({ option, param }) {
+    console.log('执行init流程', option, param);
+  },
 };
